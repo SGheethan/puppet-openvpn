607aebec11032e6fc160d5abd2b2c3aa67ef7dc7
Gheethan <arunachs@ifi.uio.no>
2019-07-09 12:16:18 +0200

    Update ca.pp

--- manifests/ca.pp
+++ manifests/ca.pp
@@ -75,7 +75,7 @@ define openvpn::ca (
   file { "${etc_directory}/openvpn/${name}/easy-rsa/easyrsa" :
     ensure  => file,
     mode    => '0740',
-    group   => 'openvpn'
+    group   => 'openvpn',
     require => File["${etc_directory}/openvpn/${name}/easy-rsa"],
   }
 
